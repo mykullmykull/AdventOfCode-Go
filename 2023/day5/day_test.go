cb38fc162b490ebeb408c8e19a92a8545819035f
mykullmykull <61916193+mykullmykull@users.noreply.github.com>
2023-12-05 16:12:50 -0700

    passed day 5 (#5)
    Co-authored-by: Michael Parkes <mparkes@axon.com>

---
+++ 2023/day5/day_test.go
@@ -0,0 +1,18 @@
+package day5
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_runA(t *testing.T) {
+	assert.Equal(t, expected, runA(testInput))
+	fmt.Printf("day5a: %d\n", runA(realInput))
+}
+
+func Test_runB(t *testing.T) {
+	assert.Equal(t, expected2, runB(testInput))
+	fmt.Printf("day5b: %d\n", runB(realInput))
+}
