8d29fb26616a27c0cacbcdf04270ffd169ca73f8
Michael Parkes <mparkes@axon.com>
2023-12-06 23:45:24 -0700

    uncommenting tests

--- 2023/day7/day_test.go
+++ 2023/day7/day_test.go
@@ -3,14 +3,16 @@ package day7
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_runA(t *testing.T) {
-	// assert.Equal(t, expected, runA(testInput))
-	// fmt.Printf("day7a: %d\n", runA(realInput))
+	assert.Equal(t, expected, runA(testInput))
+	fmt.Printf("day7a: %d\n", runA(realInput))
 }
 
 func Test_runB(t *testing.T) {
-	// assert.Equal(t, expected2, runB(testInput))
+	assert.Equal(t, expected2, runB(testInput))
 	fmt.Printf("day7b: %d\n", runB(realInput))
 }
