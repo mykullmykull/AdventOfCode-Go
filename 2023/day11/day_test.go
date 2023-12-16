f2beb031ea6791971c9178388cc8dbb3da768a7b
Michael Parkes <mparkes@axon.com>
2023-12-15 17:35:38 -0700

    2023 passed day11b

--- 2023/day11/day_test.go
+++ 2023/day11/day_test.go
@@ -3,14 +3,18 @@ package day11
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_runA(t *testing.T) {
 	// assert.Equal(t, expected, runA(testInput))
-	fmt.Printf("day1a: %d\n", runA(realInput))
+	// fmt.Printf("day1a: %d\n", runA(realInput))
 }
 
-// func Test_runB(t *testing.T) {
-// 	assert.Equal(t, expected2, runB(testInput))
-// 	fmt.Printf("day1a: %d\n", runB(realInput))
-// }
+func Test_runB(t *testing.T) {
+	assert.Equal(t, expected, runB(testInput, 2))
+	assert.Equal(t, expected2, runB(testInput, 10))
+	assert.Equal(t, expected3, runB(testInput, 100))
+	fmt.Printf("day1a: %d\n", runB(realInput, 1000000))
+}
--- 2023/day11/part2.go
+++ 2023/day11/part2.go
@@ -1,5 +1,68 @@
 package day11
 
-func runB(input []string) int {
-	return 0
+import (
+	"strings"
+)
+
+func runB(input []string, expansionFactor int) int {
+	sum := 0
+	galaxies, emptyRows, emptyCols := parse(input)
+
+	expansionFactor -= 1
+	for i, g := range galaxies {
+		for j, g2 := range galaxies {
+			if i <= j {
+				continue
+			}
+
+			distance := abs(g.row-g2.row) + abs(g.col-g2.col)
+			distance = increaseByExpansionFactor(distance, g.row, g2.row, emptyRows, expansionFactor)
+			distance = increaseByExpansionFactor(distance, g.col, g2.col, emptyCols, expansionFactor)
+			sum += distance
+		}
+	}
+	return sum
+}
+
+func parse(input []string) ([]point, []int, []int) {
+	galaxies := findAllGalaxies(input)
+	emptyRows := []int{}
+	for i, row := range input {
+		if strings.Contains(row, "#") {
+			continue
+		}
+		emptyRows = append(emptyRows, i)
+	}
+
+	emptyCols := []int{}
+	for i := 0; i < len(input[0]); i++ {
+		isColEmpty := true
+		for _, row := range input {
+			char := row[i]
+			if char == '#' {
+				isColEmpty = false
+				break
+			}
+		}
+		if isColEmpty {
+			emptyCols = append(emptyCols, i)
+		}
+	}
+	return galaxies, emptyRows, emptyCols
+}
+
+func abs(n int) int {
+	if n < 0 {
+		return n * -1
+	}
+	return n
+}
+
+func increaseByExpansionFactor(distance int, g int, g2 int, empties []int, expansionFactor int) int {
+	for _, rc := range empties {
+		if g < rc && g2 > rc || g > rc && g2 < rc {
+			distance += expansionFactor
+		}
+	}
+	return distance
 }
