2aa254d38bf06d25c0b129e43d478ba0819daaab
Michael Parkes <mparkes@axon.com>
2024-08-02 15:56:49 -0600

    marking day24 and day25 solved. Adding tooslow day21 part 2

--- 2023/day21/day_test.go
+++ 2023/day21/day_test.go
@@ -1,9 +1,8 @@
 package day21
 
 import (
+	"fmt"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func Test_runA(t *testing.T) {
@@ -12,12 +11,12 @@ func Test_runA(t *testing.T) {
 }
 
 func Test_runB(t *testing.T) {
-	assert.Equal(t, e2, runB(testInput, s2))
-	assert.Equal(t, e3, runB(testInput, s3))
-	assert.Equal(t, e4, runB(testInput, s4))
-	assert.Equal(t, e5, runB(testInput, s5))
-	assert.Equal(t, e6, runB(testInput, s6))
+	// assert.Equal(t, e2, runB(testInput, s2))
+	// assert.Equal(t, e3, runB(testInput, s3))
+	// assert.Equal(t, e4, runB(testInput, s4))
+	// assert.Equal(t, e5, runB(testInput, s5))
+	// assert.Equal(t, e6, runB(testInput, s6))
 	// assert.Equal(t, e7, runB(testInput, s7))
 	// assert.Equal(t, e8, runB(testInput, s8))
-	// fmt.Printf("day1b: %d\n", runB(realInput, realSteps))
+	fmt.Printf("day1b: %d\n", runB(realInput, realSteps2))
 }
