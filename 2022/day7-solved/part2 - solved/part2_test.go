f50226d87b0480118322a4dc9130eee0db350ea4
Michael Parkes <mparkes@axon.com>
2024-08-12 17:25:40 -0600

    solved day 7

---
+++ 2022/day7-solved/part2 - solved/part2_test.go
@@ -0,0 +1,15 @@
+package day
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPart1(t *testing.T) {
+	fmt.Println("Testing part1...") // keep this line to maintain imports
+	assert.True(t, true)            // keep this line to maintain imports
+	assert.Equal(t, expected, part2(testInput))
+	assert.Equal(t, realAnswer, part2(realInput))
+}
